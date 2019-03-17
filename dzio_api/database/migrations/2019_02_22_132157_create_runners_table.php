bb176e54b5bf2cea235bfc0ab6cf3fc06affe42d
artash <formyfreelance@gmail.com>
2019-03-17 14:49:03 +0100

    [WIP]  - Added migration for runner

--- dzio_api/database/migrations/2019_02_22_132157_create_runners_table.php
+++ dzio_api/database/migrations/2019_02_22_132157_create_runners_table.php
@@ -13,8 +13,22 @@ class CreateRunnersTable extends Migration
      */
     public function up()
     {
+        //@TODO fix types
         Schema::create('runners', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('breed');
+            $table->string('sex');
+            $table->string('age');
+            $table->string('dress');
+            $table->string('birthday');
+            $table->string('color');
+            $table->string('father');
+            $table->string('mother');
+            $table->string('owner');
+            $table->string('coach');
+            $table->string('jokey');
+            $table->string('farmer');
             $table->timestamps();
         });
     }
