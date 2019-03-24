dd4d019d4470266e58f6b6d2f79a96b88822a290
artash <formyfreelance@gmail.com>
2019-03-24 13:45:31 +0100

    [WIP] api route and controller added

---
+++ dzio_api/app/Http/Controllers/ReunionController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reunion;
+use Illuminate\Http\Request;
+
+class ReunionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        return json_encode(Reunion::where("DATE(date)", "=", "DATE(NOW())")->get());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        exit("qaqqaqaqa");
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        exit("qaqqaqaqa");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Reunion  $reunion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reunion $reunion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Reunion  $reunion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Reunion $reunion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Reunion  $reunion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reunion $reunion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Reunion  $reunion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reunion $reunion)
+    {
+        //
+    }
+}
