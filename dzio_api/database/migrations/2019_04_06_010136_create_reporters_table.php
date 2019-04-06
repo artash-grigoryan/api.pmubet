58ad186e14bd96139922c455e4cef4ee12931804
nicolascramail <C@rtm@n#7b>
2019-04-06 06:09:20 +0200

    predictions !!!

---
+++ dzio_api/database/migrations/2019_04_06_010136_create_reporters_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reporters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('societe');
+            $table->string('reporter');
+            $table->string('nb_pts')->nullable();
+            $table->string('reporter_rank')->nullable();
+            $table->integer("raceId")->unsigned();
+
+            $table->foreign('raceId')
+                ->references('id')
+                ->on('races')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reporters');
+    }
+}
