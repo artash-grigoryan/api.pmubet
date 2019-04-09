3956fa5743273ac7330cb85635d8de97125d131c
artash <formyfreelance@gmail.com>
2019-04-10 01:50:24 +0200

    [WIP] Fixed dockerfile for nodejs

--- dzio_api/resources/views/welcome.blade.php
+++ dzio_api/resources/views/welcome.blade.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
-<html lang = "en">
+<html lang="en">
 
 <head>
-    <meta charset = "UTF-8">
-    <title>Dzio</title>
+    <meta charset="UTF-8">
+    <title>Dzio.am</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-<div id = "app"></div>
+<div id="app"></div>
 <script>
     // WARNING: See the following for security issues around embedding JSON in HTML:
     // http://redux.js.org/recipes/ServerRendering.html#security-considerations
     window.__PRELOADED_STATE__ = '{}'.replace(/</g, '\\u003c');
 </script>
-<script src = "js/bundle.js"></script>
+<script src="js/bundle.js"></script>
 </body>
 
 </html>
