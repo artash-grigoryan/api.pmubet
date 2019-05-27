db2af97228cbcf4033b8397e80311be504d69d51
nicolascramail <C@rtm@n#7b>
2019-05-27 02:04:42 +0200

    fix date selector

--- dzio_website/services/reunion.js
+++ dzio_website/services/reunion.js
@@ -3,9 +3,15 @@ import axios  from './../helpers/axios';
 
 export const reunionService = {
     getAll,
+    get,
 };
 
 function getAll() {
 
     return axios.get('/api/reunion/all');
+}
+
+function get(date) {
+
+    return axios.get('/api/reunion/'+date);
 }
