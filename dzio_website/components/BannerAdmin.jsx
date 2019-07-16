848f8029e62880962222ee356f1cde3bf00b7ef0
artash <formyfreelance@gmail.com>
2019-07-16 08:21:51 +0200

    Banner fix

--- dzio_website/components/BannerAdmin.jsx
+++ dzio_website/components/BannerAdmin.jsx
@@ -40,7 +40,7 @@ export default class Banner extends React.Component {
     render() {
 
         return <div>
-            { this.state.text &&
+            { this.state.text && (new Date(this.state.date)) > (new Date()) &&
             <div className="inner-banner additional " style={{backgroundImage: "url('/img/banner/" + this.state.image + "')"}}>
                 <div className="container">
                     <div className="text-holder" style={{paddingTop: '20px'}}>
@@ -49,7 +49,7 @@ export default class Banner extends React.Component {
                         </h1>
 
                             <Countdown
-                                date={'2019-09-10'}
+                                date={this.state.date}
                                 renderer={this.renderer}
                             />
                     </div>
