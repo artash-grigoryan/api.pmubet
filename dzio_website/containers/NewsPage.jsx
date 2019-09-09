3ce992d6cedf88b78bb04fda7a55e09da4c58787
artash <formyfreelance@gmail.com>
2019-09-09 02:05:43 +0200

    Removed protocol from iframe url

--- dzio_website/containers/NewsPage.jsx
+++ dzio_website/containers/NewsPage.jsx
@@ -37,7 +37,7 @@ export default class NewsPage extends Component {
             <div className="container-fluid">
                 <div id="wrapper">
                     <div id="main">
-                        <iframe src="http://35.180.105.76:9000/" frameBorder="0" width={'100%'} height={'100%'}
+                        <iframe src="//35.180.105.76:9000/" frameBorder="0" width={'100%'} height={'100%'}
                                 frameBorder={0}></iframe>
                     </div>
                 </div>
