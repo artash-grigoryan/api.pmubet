a4911388f6b4676b83e254e05e0096c60ff918ce
artash <formyfreelance@gmail.com>
2020-08-06 23:41:22 +0200

    testing CI/CD

--- deploy.php
+++ deploy.php
@@ -57,6 +57,7 @@ task('deploy', [
     'artisan:config:cache', // | Laravel specific steps
     'artisan:optimize',     // |
     'artisan:migrate',      // |
+    'artisan key:generate',      // |
     'deploy:symlink',
     'deploy:unlock',
     'cleanup',
