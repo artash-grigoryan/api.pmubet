626a5f11f6e68b313814c0296dae5a2992749214
artash <formyfreelance@gmail.com>
2021-02-16 17:45:02 +0100

    testing CI/CD

--- api/deploy.sh
+++ api/deploy.sh
@@ -8,14 +8,15 @@ SHARED_PATH=/var/www/api.pmubet.com/shared
 # remove old releases
 echo "Sharing images"
 
-while [ $(ls -1 $PATH_RELEASES | wc -l) -gt $KEEP_RELEASES ]; do
-    oldest=$(ls -1 $PATH_RELEASES | head -1)
-    echo "  Copying $oldest"
-    cp -R "$PATH_RELEASES/$oldest/public/img" "$SHARED_PATH"
+for d in */; do
+
+    echo "  Copying $d"
+    cp -R "$PATH_RELEASES/$d/public/img" "$SHARED_PATH"
 done
 
 # move "current" link to current release
 echo "copying shared"
+rm -rf "$API_PATH/current/public/img"
 ln -s "$SHARED_PATH/img" "$API_PATH/current/public"
 
 echo ""
